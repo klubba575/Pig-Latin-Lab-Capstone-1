09043b3dd2af6b89f9df1a80626942b327792145
klubba575 <wilderandrew9@gmail.com>
2019-10-22 08:51:09 -0400

    Initial build of Pig Latin

---
+++ NewPigLatinProject/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace NewPigLatinProject
+{
+	class Program
+	
+	{
+		static string vowels = "aeiou";
+		static char[] chars = vowels.ToCharArray();
+		string word;
+		
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Welcome to the Pig Latin Translator!");
+			 GetFirstVowelIndex();
+
+		}
+		public static int GetFirstVowelIndex()
+		{
+			Console.WriteLine("Please enter a word");
+			string word = Console.ReadLine().ToLower();
+			int indexOfFirstVowel = 0;
+			for (int i = 0; i < vowels.Length; i++)
+			{
+				indexOfFirstVowel = i;
+				
+			}return indexOfFirstVowel;
+		}
+		public static string FirstLetterIsVowel(string word, int indexOfFirstVowel)
+		{
+			if ((indexOfFirstVowel) == 0)
+			{
+
+			}
+			
+		}
+	}
+}
